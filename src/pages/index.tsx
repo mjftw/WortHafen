0cc54d7dc2a57b29e3e5a530b55ba24aaf71eaf3
Merlin Webster <mjftwebster@gmail.com>
2024-01-07 23:16:32 +0000

    Updated links and added background

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,33 +14,27 @@ export default function Home() {
         <meta name="description" content="My German dictionary" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className=" flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+      <main className=" flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#7c4031] to-[#552424]">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-            Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
+            WortHafer
           </h1>
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
             <Link
               className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-              href="https://create.t3.gg/en/usage/first-steps"
+              href="https://github.com/mjftw/WortHafen"
               target="_blank"
             >
-              <h3 className="text-2xl font-bold">First Steps →</h3>
-              <div className="text-lg">
-                Just the basics - Everything you need to know to set up your
-                database and authentication.
-              </div>
+              <h3 className="text-2xl font-bold">The code</h3>
+              <div className="text-lg">Check out the project on Github</div>
             </Link>
             <Link
               className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-              href="https://create.t3.gg/en/introduction"
+              href="https://chat.openai.com/g/g-HLOArtqTc-worthafen"
               target="_blank"
             >
-              <h3 className="text-2xl font-bold">Documentation →</h3>
-              <div className="text-lg">
-                Learn more about Create T3 App, the libraries it uses, and how
-                to deploy it.
-              </div>
+              <h3 className="text-2xl font-bold">The AI</h3>
+              <div className="text-lg">Talk the AI on ChatGPT</div>
             </Link>
           </div>
           <div className="flex flex-col items-center gap-2">
