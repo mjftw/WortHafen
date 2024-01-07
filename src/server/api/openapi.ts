90fa50b42fd610b46acaa1ed0085b01353c54002
Merlin Webster <mjftwebster@gmail.com>
2024-01-07 23:29:13 +0000

    Set up OpenAPI to point to correct URL

--- src/server/api/openapi.ts
+++ src/server/api/openapi.ts
@@ -1,8 +1,9 @@
 import { generateOpenApiDocument } from "trpc-openapi";
 import { appRouter } from "./root";
+import { env } from "~/env";
 
 export const openApiDocument = generateOpenApiDocument(appRouter, {
   title: "WortHafen API",
   version: "1.0.0",
-  baseUrl: "/api",
+  baseUrl: `${env.OWN_SERVER_URL}/api`,
 });
